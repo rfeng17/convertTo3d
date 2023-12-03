6231119c66344d807679ddb79307851b546b71a7
Ray Feng <72717665+rfeng17@users.noreply.github.com>
2023-12-03 16:12:44 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,15 +4,16 @@ import subprocess
 # For Windows only
 
 # Command to run in the Anaconda Command Prompt
-command_to_run = 'C:\\Users\\raymo\\miniconda3\\Scripts\\activate.bat'
+command_to_run = 'path to\\miniconda3\\Scripts\\activate.bat'
 command_activate = "conda activate ldm"
-command_folder = 'cd C:\\Users\\raymo\\Documents\\PythonProjects\\3Dmodelai\\stable-diffusion'
-prompt_file = 'C:\\Users\\raymo\\Documents\\PythonProjects\\3Dmodelai\\prompts.txt'
+command_folder = 'cd path to\\stable-diffusion'
+prompt_file = 'path to\\prompts.txt'
 
 get_prompt = input("Enter prompt (use underscores instead of spaces): ")
 with open(prompt_file, "w") as f:
   f.write(get_prompt)
-command_prompt = "python scripts/txt2img.py --from-file C:/Users/raymo/Documents/PythonProjects/3Dmodelai/prompts.txt --plms --ckpt sd-v1-4.ckpt --skip_grid --n_samples 1"
+#Change path-to/prompts.txt to prompt file path
+command_prompt = "python scripts/txt2img.py --from-file path-to/prompts.txt --plms --ckpt sd-v1-4.ckpt --skip_grid --n_samples 1"
 
 # Create the command to open the Anaconda Command Prompt and run the desired command
 full_command = f"{command_to_run} && {command_activate} && {command_folder} &&{command_prompt}"
